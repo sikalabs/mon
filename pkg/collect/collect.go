802b0c7ddf296f1b5465d1a2d5990fa97c4cf9a0
Ondrej Sika <ondrej@ondrejsika.com>
2024-05-16 21:48:16 +0200

    feat(pkg/collect): Create HostInfo with free root disk space info (for testing)

---
+++ pkg/collect/collect.go
@@ -0,0 +1,32 @@
+package collect
+
+import (
+	"fmt"
+
+	"github.com/sikalabs/mon/pkg/collect/disk"
+)
+
+type HostInfo struct {
+	Disk disk.DataInfo
+}
+
+func GetHostInfo() (HostInfo, error) {
+	diskInfo, err := disk.GetDiskInfo()
+	if err != nil {
+		return HostInfo{}, err
+	}
+
+	return HostInfo{
+		Disk: diskInfo,
+	}, nil
+}
+
+func PrintHostInfo() error {
+	info, err := GetHostInfo()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("disk_root_free_gb=%.2f\n", info.Disk.RootFreeGB)
+	fmt.Printf("disk_root_percent=%.2f\n", info.Disk.RootFreePercent)
+	return nil
+}
---
+++ pkg/collect/disk/disk.go
@@ -0,0 +1,48 @@
+package disk
+
+import "syscall"
+
+type DataInfo struct {
+	RootFreeGB float64
+	RootFreePercent float64
+}
+
+func GetDiskInfo() (DataInfo, error) {
+	freeSpaceGB, freeSpacePercentage, err := getFreeSpaceGB("/")
+	if err != nil {
+		return DataInfo{}, err
+	}
+
+	return DataInfo{
+		RootFreeGB: freeSpaceGB,
+		RootFreePercent: freeSpacePercentage,
+	}, nil
+}
+
+func getFreeSpaceGB(path string) (float64,float64, error) {
+	var stat syscall.Statfs_t
+
+	// Get the filesystem statistics
+	err := syscall.Statfs(path, &stat)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	// Calculate the free space in bytes
+	freeSpaceBytes := stat.Bavail * uint64(stat.Bsize)
+
+	// Calculate the total space in bytes
+	totalSpaceBytes := stat.Blocks * uint64(stat.Bsize)
+
+	// Calculate the used space in bytes
+	usedSpaceBytes := totalSpaceBytes - (stat.Bfree * uint64(stat.Bsize))
+
+	// Convert the free space to gigabytes
+	const bytesInGB = 1024 * 1024 * 1024
+	freeSpaceGB := float64(freeSpaceBytes) / bytesInGB
+
+	// Calculate the used free percentage
+	freeSpacePercentage := (1 - (float64(usedSpaceBytes) / float64(totalSpaceBytes))) * 100
+
+	return freeSpaceGB, freeSpacePercentage, nil
+}
