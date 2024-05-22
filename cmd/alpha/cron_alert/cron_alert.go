0cf3466a07732825d532a23f2048abd5e76dcabe
Ondrej Sika <ondrej@ondrejsika.com>
2024-05-22 13:12:18 +0200

    feat(cmd/alpha/cron_alert): Create alerting command (print & send email)

--- cmd/alpha/cron_alert/cron_alert.go
+++ cmd/alpha/cron_alert/cron_alert.go
@@ -2,8 +2,13 @@ package cron_alert
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/sikalabs/mon/cmd/alpha"
+	"github.com/sikalabs/mon/pkg/alert"
+	"github.com/sikalabs/mon/pkg/config"
+	"github.com/sikalabs/mon/pkg/notify"
 	"github.com/spf13/cobra"
 )
 
@@ -12,10 +17,27 @@ var Cmd = &cobra.Command{
 	Short: "Run alerting for cron",
 	Args:  cobra.NoArgs,
 	Run: func(c *cobra.Command, args []string) {
-		fmt.Println("TODO")
+		config := config.LoadConfig()
+		hostname, err := os.Hostname()
+		handleError(err)
+
+		alerts, err := alert.GetAlert()
+		handleError(err)
+
+		body := alert.SprintAlerts(alerts)
+		fmt.Print(body)
+
+		err = notify.SendEmailNotification(config, hostname, body)
+		handleError(err)
 	},
 }
 
 func init() {
 	alpha.Cmd.AddCommand(Cmd)
+}
+
+func handleError(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
