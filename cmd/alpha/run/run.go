f752edfc60e9eec636f867d114fec9e187d2ad37
Ondrej Sika <ondrej@ondrejsika.com>
2026-02-17 23:20:35 +0100

    feat(pkg/run): Move implementation from cmd/alpha/run to pkg/run RunOrDie() func

--- cmd/alpha/run/run.go
+++ cmd/alpha/run/run.go
@@ -1,16 +1,8 @@
 package run
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/sikalabs/mon/cmd/alpha"
-	"github.com/sikalabs/mon/pkg/alert"
-	"github.com/sikalabs/mon/pkg/alert_v2"
-	"github.com/sikalabs/mon/pkg/config"
-	"github.com/sikalabs/mon/pkg/http_checks"
-	"github.com/sikalabs/mon/pkg/notify"
-	"github.com/sikalabs/slu/pkg/utils/error_utils"
+	"github.com/sikalabs/mon/pkg/run"
 	"github.com/spf13/cobra"
 )
 
@@ -19,40 +11,7 @@ var Cmd = &cobra.Command{
 	Short: "Run alerting for cron",
 	Args:  cobra.NoArgs,
 	Run: func(c *cobra.Command, args []string) {
-		config := config.LoadConfig()
-		hostname, err := os.Hostname()
-		error_utils.HandleError(err)
-
-		alerts, err := alert.GetAlert()
-		error_utils.HandleError(err)
-
-		// Alerts (v1) to AlertsV2
-		var alertsV2 []alert_v2.AlertV2
-		for _, alert := range alerts {
-			alertsV2 = append(alertsV2, alert_v2.AlertV2{
-				CheckType: "host-info",
-				CheckName: alert.Name,
-				OK:        alert.CurrentValueFloat64 > alert.LimitValueFloat64,
-				ErrorMessage: fmt.Sprintf(
-					"current=%.2f is lower than limit=%.2f",
-					alert.CurrentValueFloat64,
-					alert.LimitValueFloat64,
-				),
-			})
-		}
-
-		_, http_alerts := http_checks.RunHttpChecks(config)
-		alertsV2 = append(alertsV2, http_alerts...)
-
-		body := alert_v2.SprintAlertsV2(alertsV2)
-
-		fmt.Print(body)
-
-		err = notify.SendEmailNotification(config, hostname, body)
-		error_utils.HandleError(err)
-
-		err = notify.SendTelegramNotification(config, hostname, body)
-		error_utils.HandleError(err)
+		run.RunOrDie()
 	},
 }
 
