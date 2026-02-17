17abaec01f59c1040b677012e33a35f5bb4e38f6
Ondrej Sika <ondrej@ondrejsika.com>
2026-02-17 22:09:33 +0100

    feat(pkg/config): Add Telegram to config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"strconv"
 	"strings"
 
 	"github.com/spf13/viper"
@@ -17,6 +18,11 @@ type Config struct {
 			SMTPUsername  string
 			SMTPPassword  string
 			SMTPEmailFrom string
+			EmailsTo      []string
+		}
+		Telegram struct {
+			Token   string
+			ChatIDs []int64
 		}
 	}
 }
@@ -30,17 +36,36 @@ func LoadConfig() Config {
 	viper.SetEnvPrefix("mon")
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
+	// email
 	viper.BindEnv(MON_NOTIFICATIONS_EMAIL_SMTP_HOST)
 	viper.BindEnv(MON_NOTIFICATIONS_EMAIL_SMTP_PORT)
 	viper.BindEnv(MON_NOTIFICATIONS_EMAIL_SMTP_USERNAME)
 	viper.BindEnv(MON_NOTIFICATIONS_EMAIL_SMTP_PASSWORD)
 	viper.BindEnv(MON_NOTIFICATIONS_EMAIL_SMTP_EMAIL_FROM)
+	viper.BindEnv(MON_NOTIFICATIONS_EMAIL_EMAILS_TO)
+
+	// telegram
+	viper.BindEnv(MON_NOTIFICATIONS_TELEGRAM_TOKEN)
+	viper.BindEnv(MON_NOTIFICATIONS_TELEGRAM_CHAT_IDS)
 
 	var c Config
+
+	// email
 	c.Notifications.Mail.SMTPHost = viper.GetString(MON_NOTIFICATIONS_EMAIL_SMTP_HOST)
 	c.Notifications.Mail.SMTPPort = viper.GetInt(MON_NOTIFICATIONS_EMAIL_SMTP_PORT)
 	c.Notifications.Mail.SMTPUsername = viper.GetString(MON_NOTIFICATIONS_EMAIL_SMTP_USERNAME)
 	c.Notifications.Mail.SMTPPassword = viper.GetString(MON_NOTIFICATIONS_EMAIL_SMTP_PASSWORD)
 	c.Notifications.Mail.SMTPEmailFrom = viper.GetString(MON_NOTIFICATIONS_EMAIL_SMTP_EMAIL_FROM)
+	c.Notifications.Mail.EmailsTo = viper.GetStringSlice(MON_NOTIFICATIONS_EMAIL_EMAILS_TO)
+
+	// telegram
+	c.Notifications.Telegram.Token = viper.GetString(MON_NOTIFICATIONS_TELEGRAM_TOKEN)
+	for _, s := range viper.GetStringSlice(MON_NOTIFICATIONS_TELEGRAM_CHAT_IDS) {
+		id, err := strconv.ParseInt(s, 10, 64)
+		if err == nil {
+			c.Notifications.Telegram.ChatIDs = append(c.Notifications.Telegram.ChatIDs, id)
+		}
+	}
+
 	return c
 }
--- pkg/config/config_const.go
+++ pkg/config/config_const.go
@@ -7,4 +7,8 @@ const (
 	MON_NOTIFICATIONS_EMAIL_SMTP_USERNAME   = "notifications.mail.smtp_username"
 	MON_NOTIFICATIONS_EMAIL_SMTP_PASSWORD   = "notifications.mail.smtp_password"
 	MON_NOTIFICATIONS_EMAIL_SMTP_EMAIL_FROM = "notifications.mail.smtp_email_from"
+	MON_NOTIFICATIONS_EMAIL_EMAILS_TO       = "notifications.mail.emails_to"
+
+	MON_NOTIFICATIONS_TELEGRAM_TOKEN    = "notifications.telegram.token"
+	MON_NOTIFICATIONS_TELEGRAM_CHAT_IDS = "notifications.telegram.chat_ids"
 )
